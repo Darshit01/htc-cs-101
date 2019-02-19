c2d6f2dce5723200c4c807279eca4f0ae0d8affb
User <User@TECH-018435.OnBase.net>
2019-02-19 17:28:22 -0500

    Week1/2

--- Week01/Program.cs
+++ Week01/Program.cs
@@ -7,7 +7,14 @@ namespace Week01
         static void Main(string[] args)
         {
         Console.WriteLine("Hello Word");
-            // say Hello World here
+        string userName = "Darsh";
+        int userAge = 18;
+        double numPets = 3.14;
+        string schoolAttended = "NONE";
+        Console.WriteLine("My name is " + userName);
+        Console.WriteLine("I am "+ userAge + "old");
+        Console.WriteLine("I have " + numPets+ "pets");
+        Console.WriteLine("School Attended "+ schoolAttended);
             
         }
     }
