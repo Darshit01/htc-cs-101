09c1eda64dba8553b9e54f8f566253b8bac51b02
User <User@TECH-018435.OnBase.net>
2019-03-05 18:42:16 -0500

    asdfasf

---
+++ Studentwork/Week5/Calculator.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Week5{
+    class calculator{
+         public static int MultiplyNumber( int integer1, int integer2){
+            return integer1 * integer2;
+
+        }
+
+        public static int DivideNumber (int divideNum1, int divideNum2 ){
+            
+        }
+
+
+        public static void Main (string[] args){
+
+          // int resutofMultplication =  MultiplyNumber(2, 3);
+           
+        }    
+       
+    
+    
+    }
+}
