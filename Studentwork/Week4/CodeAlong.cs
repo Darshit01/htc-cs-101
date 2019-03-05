7135bd994d23b9ba25b00c8ccce574f30d451951
User <User@TECH-018435.OnBase.net>
2019-03-05 18:25:38 -0500

    program

---
+++ Studentwork/Week4/CodeAlong.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Week4
+{
+    class CodeAlong
+    
+    {
+        static void Main5(string[] args)
+        {
+            /* 
+            bool snowOnGround = false;
+            bool foundGloves =  true;
+           
+            if (snowOnGround == true && foundGloves == true){
+                Console.WriteLine("I am going to build a snoman");
+            }else{
+                Console.WriteLine(" I am staying inside");
+            }
+            */
+
+            bool snowOnGround = true;
+            bool feelingSick = true;
+            if (snowOnGround == true || feelingSick == true){
+                
+                Console.WriteLine("I am going to eat some soup");
+            }else{
+                Console.WriteLine("I don't want soup");
+            }
+
+        }
+    }
+}
