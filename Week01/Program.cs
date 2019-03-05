1da427ab201844e87003b54b1f36e7ed1dbd4442
User <User@TECH-018435.OnBase.net>
2019-03-05 18:41:13 -0500

    week1

--- Week01/Program.cs
+++ Week01/Program.cs
@@ -4,7 +4,7 @@ namespace Week01
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main3(string[] args)
         {
         Console.WriteLine("Hello Word");
         string userName = "Darsh";
