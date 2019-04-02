a2dc8800e0cf96509cbc9a93a069bf38a918660f
User <User@TECH-018435.OnBase.net>
2019-04-02 17:33:44 -0400

    Week7 Challenges

--- Studentwork/Week7/FloydsTriangle.cs
+++ Studentwork/Week7/FloydsTriangle.cs
@@ -1,16 +1,21 @@
 using System;
-namespace Week7_Floyd{
-    class Floyd{
-        public static void Main1(string[] args){
+namespace Week7_Floyd
+{
+    class Floyd
+    {
+        public static void Main1(string[] args)
+        {
             Console.WriteLine("Enter the number of lines you want to print");
             int userInput = Convert.ToInt32(Console.ReadLine());
 
             int x = 1;
-            for( int i = 0; i <= userInput; i++){
-                for (int j = 0; j <=i; j++){
+            for (int i = 0; i <= userInput; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
                     Console.WriteLine(x + " ");
                     x++;
-                    
+
                 }
             }
 
--- Studentwork/Week7/PrimeNumber.cs
+++ Studentwork/Week7/PrimeNumber.cs
@@ -1,26 +1,33 @@
 using System;
-namespace Week7_PrimeNumber{
-    class PrimeNumber{
-        public static void Main5(string[] args){
+namespace Week7_PrimeNumber
+{
+    class PrimeNumber
+    {
+        public static void Main5(string[] args)
+        {
 
             Console.WriteLine("Enter the number");
             int userInput = Convert.ToInt32(Console.ReadLine());
 
-          //  int x = 0;
-            for (int i = 2; i <= userInput/2; i++){
-                if (userInput % i == 0){
-                   
+            //  int x = 0;
+            for (int i = 2; i <= userInput / 2; i++)
+            {
+                if (userInput % i == 0)
+                {
+
                     Console.WriteLine(userInput + " is not prime");
-                    
-                    
-                }else{
+
+
+                }
+                else
+                {
                     Console.WriteLine(userInput + " is prime");
                     break;
                 }
-            //     x++;
+                //     x++;
             }
 
-            
+
         }
     }
 }
