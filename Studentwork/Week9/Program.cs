eb15cc8cff1d3f0d9f4b5d52b153c8d36a2145c9
User <User@TECH-018435.OnBase.net>
2019-04-09 19:26:57 -0400

    Week9

--- Studentwork/Week9/Program.cs
+++ Studentwork/Week9/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace Week9
 {
     class Program
     {
-        static void Main(string[] args)
+
+        static void Main1(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> toDoList = new List<string>();
+            while (true)
+            {
+                Console.WriteLine("Enter a command:");
+                string command = Console.ReadLine();
+                if (command == "Add")
+                {
+                    Console.WriteLine("Enter an item: ");
+                    string item = Console.ReadLine();
+                    toDoList.Add(item);
+                    
+
+                }
+                else if (command == "View")
+                {
+                    ViewList(toDoList);
+                }
+                else if (command == "Exit")
+                {
+                    break;
+                }
+            }
+        }
+
+        public static void ViewList(List<string> str)
+        {
+            int x = str.Count;
+            
+            Console.WriteLine(x);
+            Console.WriteLine("To do: ");
+            foreach (string item in str){
+                Console.WriteLine("-" + item);
+                
+
+            }
         }
     }
 }
---
+++ Studentwork/Week9/additionalChallenges.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+
+namespace challenge
+{
+    class challenges
+    {
+        public static void Main(string[] args)
+        {
+            addToList();
+            addNames();
+            palindromeCheck();
+        }
+
+        public static void addToList()
+        {
+            List<int> myList = new List<int>();
+            myList.Add(2);
+            myList.Add(4);
+            myList.Add(6);
+            myList.Add(8);
+            myList.Add(10);
+            myList.ForEach(Console.WriteLine);
+        }
+
+        public static void addNames()
+        {
+            List<string> str = new List<string>();
+            str.Add("Alice");
+            str.Add("Bob");
+            str.Add("Charles");
+            str.Add("Danielle");
+            str.Add("Edgar");
+            str.Add("Frannie");
+            Console.WriteLine("Enter a name to add: ");
+            string input = Console.ReadLine();
+            str.Add(input);
+            str.ForEach(Console.WriteLine);
+
+            Console.WriteLine("Enter a name: ");
+            string uInput = Console.ReadLine();
+            foreach (string i in str)
+            {
+                if (uInput == i)
+                {
+                    Console.WriteLine(uInput + "already exists");
+                }
+                else
+                {
+                    Console.WriteLine(uInput + " is not in the list");
+
+                }
+            }
+        }
+
+
+        public static void palindromeCheck()
+        {
+            string x, y = "";
+            Console.WriteLine("What is the string");
+            x = Console.ReadLine();
+
+            int length = x.Length;
+
+            for (int i = length - 1; i >= 0; i--)
+            {
+                y = y + x[i];
+
+
+            }
+            if (x.Equals(y))
+            {
+                Console.WriteLine("palindrome");
+            }
+            else
+            {
+                Console.WriteLine("not a palindrome");
+            }
+        }
+    }
+
+}
+
+
+
+
+
